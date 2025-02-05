b75e9b3bc21182bb261a1e78c4ec9da400554911
Yasmine-Bassem <yassminebassem991@gmail.com>
2025-02-06 01:46:42 +0200

    Add pdf

--- js/lessons.js
+++ js/lessons.js
@@ -12,3 +12,7 @@ menuToggle.addEventListener('click', () => {
   // التبديل بين إظهار وإخفاء القائمة
   menu.classList.toggle('show');
 });
+
+    function openPDF() {
+        window.open("./Nemmy Platform.pdf", "_blank");
+    }
