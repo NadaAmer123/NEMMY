d86d7de9431e57c84fc1af297b307d494e0900f1
Yasmine-Bassem <yassminebassem991@gmail.com>
2025-02-06 09:22:29 +0200

    back button

--- js/science.js
+++ js/science.js
@@ -12,42 +12,41 @@ function openPDF() {
   window.open("./Nemmy Platform.pdf", "_blank");
 }
 
-
-
-// قراءة البارامتر من الـ URL
+var coursesSection = document.getElementById("Courses");
+var lessonsecSection = document.getElementById("lessonsec");
 function toggleSection() {
-
-  var coursesSection = document.getElementById('Courses');
-  var lessonsecSection = document.getElementById('lessonsec');
-  
   // إخفاء قسم الدورات
-  coursesSection.style.display = 'none';
-  
+  coursesSection.style.display = "none";
+
   // إظهار قسم الدروس
-  lessonsecSection.style.display = 'block';
+  lessonsecSection.style.display = "block";
+}
+//زر الرجوع
+function toggleSectionBack() {
+  coursesSection.style.display = "block";
+  lessonsecSection.style.display = "none";
 }
 
-
 function getSubjectFromURL() {
-    const params = new URLSearchParams(window.location.search);
-    return params.get("subject"); // ترجع اسم المادة
-  }
-  
-  // تحديث المحتوى بناءً على المادة المختارة
-  function updatePageContent() {
-    const subject = getSubjectFromURL();
-    if (!subject || !unitsData[subject]) return;
-  
-    const unitTitle = document.getElementById("unitTitle");
-    const unitAccordion = document.getElementById("unitAccordion");
-  
-    // تحديث العنوان
-    unitTitle.innerHTML = `<h3>${unitsData[subject].title}</h3>`;
-  
-    // تحديث الوحدات والدروس
-    unitAccordion.innerHTML = unitsData[subject].units
-      .map(
-        (unit, index) => `
+  const params = new URLSearchParams(window.location.search);
+  return params.get("subject"); // ترجع اسم المادة
+}
+
+// تحديث المحتوى بناءً على المادة المختارة
+function updatePageContent() {
+  const subject = getSubjectFromURL();
+  if (!subject || !unitsData[subject]) return;
+
+  const unitTitle = document.getElementById("unitTitle");
+  const unitAccordion = document.getElementById("unitAccordion");
+
+  // تحديث العنوان
+  unitTitle.innerHTML = `<h3>${unitsData[subject].title}</h3>`;
+
+  // تحديث الوحدات والدروس
+  unitAccordion.innerHTML = unitsData[subject].units
+    .map(
+      (unit, index) => `
           <div class="accordion-item">
               <h2 class="accordion-header">
                   <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" 
@@ -66,10 +65,9 @@ function getSubjectFromURL() {
               </div>
           </div>
       `
-      )
-      .join("");
-  }
-  
-  // استدعاء الدالة عند تحميل الصفحة
-  document.addEventListener("DOMContentLoaded", updatePageContent);
-  
+    )
+    .join("");
+}
+
+// استدعاء الدالة عند تحميل الصفحة
+document.addEventListener("DOMContentLoaded", updatePageContent);
